d2946e08eee36314401df47737036187ee3211ad
alejama <alejandroamayahdz@gmail.com>
2023-05-04 11:49:51 -0600

    adding ros2_control real

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -1,4 +1,3 @@
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -36,14 +35,14 @@ def generate_launch_description():
     # )
 
 
-    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
-    twist_mux = Node(
-            package="twist_mux",
-            executable="twist_mux",
-            parameters=[twist_mux_params],
-            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-        )
-    
+    # twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    # twist_mux = Node(
+    #         package="twist_mux",
+    #         executable="twist_mux",
+    #         parameters=[twist_mux_params],
+    #         remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+    #     )
+
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controller.yaml')
@@ -106,7 +105,7 @@ def generate_launch_description():
     return LaunchDescription([
         rsp,
         # joystick,
-        twist_mux,
+        # twist_mux,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
